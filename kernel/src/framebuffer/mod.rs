67305b141d465896f890b8cc524118ac94fbc7cc
Piotr Łuczak <piotrluczak1995@gmail.com>
2024-03-21 16:32:19 +0100

    Add logger & Hello World

--- kernel/src/framebuffer/mod.rs
+++ kernel/src/framebuffer/mod.rs
@@ -1,4 +1,65 @@
-use bootloader_api::info::{FrameBuffer, PixelFormat};
+use core::convert::Infallible;
+
+use bootloader_api::info::{FrameBufferInfo, PixelFormat};
+use embedded_graphics::{
+    draw_target::DrawTarget,
+    geometry::{OriginDimensions, Size},
+    pixelcolor::{Rgb888, RgbColor},
+    Pixel,
+};
+pub struct Display<'f> {
+    fb: &'f mut FrameBuffer,
+}
+
+impl<'f> Display<'f> {
+    pub fn new(fb: &'f mut FrameBuffer) -> Self {
+        Display { fb }
+    }
+
+    fn draw_pixel(&mut self, Pixel(pos, color): Pixel<Rgb888>) {
+        let info = self.fb.info();
+        let x = usize::try_from(pos.x).unwrap();
+        let y = usize::try_from(pos.y).unwrap();
+
+        if (0..info.width).contains(&x) && (0..info.height).contains(&y) {
+            set_pixel(
+                self.fb,
+                &Position { x, y },
+                &Color {
+                    r: color.r(),
+                    g: color.g(),
+                    b: color.b(),
+                },
+            )
+        }
+    }
+}
+
+impl DrawTarget for Display<'_> {
+    type Color = Rgb888;
+    type Error = Infallible;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item = Pixel<Self::Color>>,
+    {
+        for pixel in pixels {
+            self.draw_pixel(pixel)
+        }
+
+        Ok(())
+    }
+}
+
+impl OriginDimensions for Display<'_> {
+    fn size(&self) -> embedded_graphics::prelude::Size {
+        let info = self.fb.info();
+        Size::new(
+            info.width.try_into().unwrap(),
+            info.height.try_into().unwrap(),
+        )
+    }
+}
 
 #[derive(Debug)]
 pub struct Position {
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![no_main]
 mod framebuffer;
+mod logger;
 
 use core::panic::PanicInfo;
-use framebuffer::{set_pixel, Color, Position};
+use logger::init_logger;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -13,30 +14,8 @@ fn panic(_info: &PanicInfo) -> ! {
 fn start(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
     if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
         let info = framebuffer.info();
-        let green = Color { r: 0, g: 255, b: 0 };
-        let blue = Color { r: 0, g: 0, b: 255 };
-        let middle = Position {
-            x: info.width / 2,
-            y: info.height / 2,
-        };
-
-        // Draw a grid
-        for x in 0..info.width {
-            for y in 0..info.height {
-                if x % 100 == 0 || y % 100 == 0 {
-                    let pos = Position { x, y };
-                    set_pixel(framebuffer, &pos, &green);
-                }
-            }
-        }
-
-        // Draw a 20:20 box in the middle
-        for x in (middle.x - 10)..(middle.x + 10) {
-            for y in (middle.y - 10)..(middle.y + 10) {
-                let pos = Position { x, y };
-                set_pixel(framebuffer, &pos, &blue);
-            }
-        }
+        init_logger(framebuffer.buffer_mut(), info);
+        log::info!("Hello kernel!");
     }
 
     loop {}
