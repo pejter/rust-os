2301ae88d6a83020a4aa3a76590df59f18bce10a
Piotr Łuczak <piotrluczak1995@gmail.com>
2024-03-21 16:32:19 +0100

    Add logger & Hello World

---
+++ kernel/src/logger.rs
@@ -0,0 +1,12 @@
+use bootloader_api::info::FrameBufferInfo;
+use bootloader_x86_64_common::logger::LockedLogger;
+use conquer_once::spin::OnceCell;
+
+pub(crate) static LOGGER: OnceCell<LockedLogger> = OnceCell::uninit();
+
+pub(crate) fn init_logger(framebuffer: &'static mut [u8], info: FrameBufferInfo) {
+    let logger = LOGGER.get_or_init(|| LockedLogger::new(framebuffer, info, true, false));
+    log::set_logger(logger).expect("Logger already set");
+    log::set_max_level(log::LevelFilter::Trace);
+    log::info!("Logger initialised");
+}
