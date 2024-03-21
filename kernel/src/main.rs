ae21af6e001f250b5f1dcb6b78aeb24d1866b093
Piotr Łuczak <piotrluczak1995@gmail.com>
2024-03-21 16:33:40 +0100

    Add basic interrupts handling

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,21 +1,44 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
+
 mod framebuffer;
+mod interrupt;
 mod logger;
 
 use core::panic::PanicInfo;
+use interrupt::init_idt;
 use logger::init_logger;
 
+use crate::framebuffer::Display;
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    log::error!("{}", info);
     loop {}
 }
 
 fn start(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    init_idt();
+
     if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        #[cfg(not(visual))]
+        // Text mode
         let info = framebuffer.info();
         init_logger(framebuffer.buffer_mut(), info);
         log::info!("Hello kernel!");
+
+        #[cfg(visual)]
+        // Visual mode
+        let display = Display::new(framebuffer);
+    }
+
+    // Breakpoint
+    x86_64::instructions::interrupts::int3();
+
+    // Page fault
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 10;
     }
 
     loop {}
